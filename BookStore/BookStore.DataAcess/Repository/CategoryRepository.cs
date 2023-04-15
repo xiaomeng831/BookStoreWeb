a24d852d01e23a80f937ae123c467595919a3e57
xiaomeng831 <78976407+xiaomeng831@users.noreply.github.com>
2023-04-15 03:10:47 -0400

    set up the project and completed the category

---
+++ BookStore/BookStore.DataAcess/Repository/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.DataAcess.Data;
+using BookStore.DataAcess.Repository.IRepository;
+using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.DataAcess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private  ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+               
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ BookStore/BookStore.DataAcess/Repository/UnitofWork.cs
@@ -0,0 +1,27 @@
+﻿using BookStore.DataAcess.Data;
+using BookStore.DataAcess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAcess.Repository
+{
+    public  class UnitofWork : IUnitofWork
+    {
+        private ApplicationDbContext _db;
+        public UnitofWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+        public ICategoryRepository Category { get; private set; }
+        
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
