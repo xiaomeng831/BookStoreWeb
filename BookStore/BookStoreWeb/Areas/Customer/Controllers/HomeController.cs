206841301a60f593322a2e1657d87a7aabcffec5
xiaomeng831 <78976407+xiaomeng831@users.noreply.github.com>
2023-04-16 01:10:30 -0400

    completed the home page

--- BookStore/BookStoreWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookStore/BookStoreWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using BookStore.Models;
+﻿using BookStore.DataAcess.Repository;
+using BookStore.DataAcess.Repository.IRepository;
+using BookStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,18 +10,32 @@ namespace BookStoreWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitofWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitofWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
-        public IActionResult Privacy()
+		public IActionResult Details(int id)
+		{
+            ShoppingCart cartObj = new()
+            {               
+                Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType"),
+				Count = 1
+			};
+                
+            return View(cartObj);
+		}
+
+		public IActionResult Privacy()
         {
             return View();
         }
